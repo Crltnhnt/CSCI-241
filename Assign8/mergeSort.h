893d9596ab75d247deab0ad16e74bcb76eb79a92
Crltnhnt <58267536+Crltnhnt@users.noreply.github.com>
2020-05-05 12:19:27 -0500

    Update mergeSort.h

--- Assign8/mergeSort.h
+++ Assign8/mergeSort.h
@@ -1,7 +1,6 @@
 /*********************************************************************
    PROGRAM:    CSCI 241 Assignment 8
-   PROGRAMMER: Carlton Hunt & Joshua Dyra
-   LOGON ID:   z1772974 & z1798711
+   PROGRAMMER: Carlton Hunt
    DUE DATE:   11/29/2017
 
    FUNCTION:   This header file sorts the lists using sort algorithms.
