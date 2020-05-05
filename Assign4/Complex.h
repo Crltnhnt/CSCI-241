6a70b69a8c79d6b951dbade47b5595670d5add65
Crltnhnt <58267536+Crltnhnt@users.noreply.github.com>
2020-05-05 12:08:51 -0500

    Update Complex.h

--- Assign4/Complex.h
+++ Assign4/Complex.h
@@ -4,8 +4,7 @@
 /****************************************************************************
 CSCI 241 - Assignment 4 - Fall 2017
 
-Programmer:	Carlton Hunt & Joshua Dyra
-Z-ID:		z1772974 & z1798711
+Programmer:	Carlton Hunt
 Section:	3
 TA:		Bhavanna Mallieneni
 Date Due:	October 19th, 2017
